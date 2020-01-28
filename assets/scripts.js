a4693d4b8752c290738ff9a8afb4d8dda0bab0fa
David Chau <david.chau@fedex.com>
2020-01-28 13:42:16 -0500

    Added logic to populate page

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,68 +1,39 @@
 //api - 2bfac481a9f992bf81be707c9df9b308
 
-var testApi="https://api.openweathermap.org/data/2.5/forecast?q=orlando,us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
-var test1Api="https://api.openweathermap.org/data/2.5/weather?q=orlando,us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
-var userCity;
-var userCity5Search="https://api.openweathermap.org/data/2.5/forecast?q="+userCity+",us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
-var userCity1Search="https://api.openweathermap.org/data/2.5/weather?q="+userCity+",us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
-var lon=-81.38
-var lat= 28.54
-var uvCity="https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid=2bfac481a9f992bf81be707c9df9b308"
+// var testApi="https://api.openweathermap.org/data/2.5/forecast?q=orlando,us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
+// var test1Api="https://api.openweathermap.org/data/2.5/weather?q=orlando,us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
+// var userCity;
+// var userCity5Search="https://api.openweathermap.org/data/2.5/forecast?q="+userCity+",us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
+// var userCity1Search="https://api.openweathermap.org/data/2.5/weather?q="+userCity+",us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
+console.log(userCity);
+$("#searchBtn").on("click", function(){
+  var input = $("#userCity").val();
+  getCurrentWeather(input);
+  createFiveDay(input);
+});
 
+  function createFiveDay(response){
+    var userCity;
+    userCity = response;
+    var userCity5Search="https://api.openweathermap.org/data/2.5/forecast?q="+userCity+",us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
 
-//get current weather
-  //get 5 day forcast
-  $.ajax({
-    url: test1Api,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    console.log(response.weather[0].icon);
-
-    console.log(response.name);
-    console.log("temp "+convertKalvin(response.main.temp));
-    console.log(moment().format('ll'));
-    console.log("humidity "+response.main.humidity);
-    console.log("wind "+response.wind.speed);
-  });
-
-$.ajax({
-    url: testApi,
-    method: "GET"
-  }).then(function(response) {
-    //console.log(response.list);
-    Object.entries(response.list).forEach(function(item){
-        var midDay = item[1].dt_txt.substring(11);
-        if(midDay == "12:00:00"){
-            //gets day
-            console.log(item[1].dt_txt);
-            //gets temp
-            console.log("temp "+convertKalvin(item[1].main.temp));
-            //gets humidity
-            console.log("humidity "+item[1].main.humidity);
-            //gets icon
-            console.log(item[1].weather[0].icon);
-        }
-    })
-  });
-
-$.ajax({
-    url: uvCity,
-    method: "GET"
-  }).then(function(response) {
-      //uv index
-    console.log("uv "+response.value);
-    
-
-  });
-
-  function convertKalvin(temp){
-    var f = (temp * 9/5 - 459.67);
-    return Math.round(f);
-  }
-
-  function createFiveDay(){
-
+    $.ajax({
+      url: userCity5Search,
+      method: "GET"
+    }).then(function(citySelector) {
+      $("#fiveDayDiv").remove();
+      $("#fiveDay").append("<div id='fiveDayDiv'><h3>5-Day Forcast</h3></div>")
+      Object.entries(citySelector.list).forEach(function(item){
+          var midDay = item[1].dt_txt.substring(11);
+          var imgIcon = item[1].weather[0].icon;
+          if(midDay == "12:00:00"){
+            var dates = new Date(item[1].dt_txt)
+              $("#fiveDayDiv").append("<div class='float-left fiveDayBox'>"+ dates.toLocaleDateString('en-US')
+              + "<img height='50' width='50' src=http://openweathermap.org/img/wn/"+imgIcon+"@2x.png>"
+              + " Temp: "+convertKalvin(item[1].main.temp)+"°F Humidity: "+item[1].main.humidity+ "%</div>")
+          }
+      })
+    });
   }
 
   function createSearchHistory(){
@@ -70,9 +41,42 @@ $.ajax({
   }
 
   function getCurrentWeather(response){
-    
+    var userCity;
+    userCity = response;
+    var userCity1Search="https://api.openweathermap.org/data/2.5/weather?q="+userCity+",us&mode=json&appid=2bfac481a9f992bf81be707c9df9b308"
+
+    $.ajax({
+      url: userCity1Search,
+      method: "GET"
+    }).then(function(citySelector) {
+      var imgIcon = citySelector.weather[0].icon;
+      $("#currentWeather").remove();
+      getUv(citySelector);
+      $("#currentCity").append("<div id='currentWeather'></div?")
+      $("#currentWeather").append("<h3>"+citySelector.name+" "+moment().format('ll')+
+      "<img src=http://openweathermap.org/img/wn/"+imgIcon+"@2x.png></h3>");
+      $("#currentWeather").append("<p> Temperature: "+convertKalvin(citySelector.main.temp)+"°F</p>");
+      $("#currentWeather").append("<p>Humidity: "+citySelector.main.humidity+"%</p>");
+      $("#currentWeather").append("<p>Wind speed: "+citySelector.wind.speed+"MPH</p>");
+
+    });
   }
 
-  function getUv(){
+  function getUv(city){
+    var lon = city.coord.lon;
+    var lat= city.coord.lat;
+    var uvCity="https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid=2bfac481a9f992bf81be707c9df9b308"
+    $.ajax({
+      url: uvCity,
+      method: "GET"
 
+    }).then(function(response) {
+     $("#currentWeather").append("<p>UV Index: "+response.value+"</p>");
+
+    });
+  }
+
+  function convertKalvin(temp){
+    var f = (temp * 9/5 - 459.67);
+    return Math.round(f);
   }
